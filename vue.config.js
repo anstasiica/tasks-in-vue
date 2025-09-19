89e03a2643ca34afba899d710300412d75583370
anstasiica <anstasiica@gmail.com>
2025-09-19 13:43:32 +0300

    удалит лишние файлы

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-// файл vue.config.js должен быть расположен в корневом каталоге проекта
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/tasks-in-vue/'
-    : '/'
-}
